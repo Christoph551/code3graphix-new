6ecafc24a79e665b059bafa0727a7a1e8c52f0e2
Christoph551 <christophersimmonds551@gmail.com>
2024-04-20 09:14:43 -0700

    Adjusted product page

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -2,18 +2,24 @@ import { Link } from 'react-router-dom';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import Img from 'react-bootstrap/Image';
 
 export default function Products() {
     return (
-        <Container>
-            <h1>Products</h1>
-            <p>Check out our products!</p>
+        <div>
+            <header className='productPage'>
+                <h2>Products</h2>
+            </header>
+
+            <p className='productParagraph'>Check out our products!</p>
+        <Container className='productList1'>
 
             <Row>
                 <Col>
                 {/* Rather than the link being /drinkware, keep the path as /products and include an ID associated with different items. Explore also using tags for each category */}
                     <Link to="/drinkware">
-                        <img src="https://via.placeholder.com/150" alt="Drinkware" />
+                        <h2>Drinkware</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Drinkware" />
                     </Link>
                 </Col>
             </Row>
@@ -21,7 +27,8 @@ export default function Products() {
             <Row>
                 <Col>
                     <Link to="/personalized-gifts">
-                        <img src="https://via.placeholder.com/150" alt="Personalized Gifts" />
+                    <h2>Personalized Gifts</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Personalized Gifts" />
                     </Link>
                 </Col>
             </Row>
@@ -29,15 +36,18 @@ export default function Products() {
             <Row>
                 <Col>
                     <Link to="/glass-and-acrylics">
-                        <img src="https://via.placeholder.com/150" alt="Glass and Acrylics" />
+                    <h2>Glass and Acrylics</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Glass and Acrylics" />
                     </Link>
                 </Col>
             </Row>
-
+        </Container>
+        <Container className='productList2'>
             <Row>
                 <Col>
                     <Link to="/engraveable-metals">
-                        <img src="https://via.placeholder.com/150" alt="Engraveable Metals" />
+                    <h2>Engraveable Metals</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Engraveable Metals" />
                     </Link>
                 </Col>
             </Row>
@@ -45,7 +55,8 @@ export default function Products() {
             <Row>
                 <Col>
                     <Link to="/sublimation">
-                        <img src="https://via.placeholder.com/150" alt="Sublimation" />
+                    <h2>Sublimation</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Sublimation" />
                     </Link>
                 </Col>
             </Row>
@@ -53,10 +64,12 @@ export default function Products() {
             <Row>
                 <Col>
                     <Link to="/laserable-leather">
-                        <img src="https://via.placeholder.com/150" alt="Laserable Leather" />
+                    <h2>Laserable Leather</h2>
+                        <Img src="https://via.placeholder.com/150" alt="Laserable Leather" />
                     </Link>
                 </Col>
             </Row>
         </Container>
+        </div>
     )
 }
