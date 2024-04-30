028de7f76f8914bac0e891709bc743d9a17b581a
Christoph551 <christophersimmonds551@gmail.com>
2024-04-30 02:40:38 -0700

    Got conditional logic working based off a user's login. Login changes to logout and vice versa. Also added some modals to modernize the forms a bit. Creating save point. Need to continue with modals on signup form as well.

--- client/src/components/productCat/Drinkware.js
+++ client/src/components/productCat/Drinkware.js
@@ -1,38 +1,48 @@
 import React, { useState, useEffect } from 'react';
+import Img from 'react-bootstrap/Image';
 
 function Drinkware() {
     const [products, setProducts] = useState([]);
-    const [isAdmin, setIsAdmin] = useState(false); // Assuming isAdmin state represents the user's role
 
     useEffect(() => {
         fetch('http://localhost:3001/api/products')
             .then(response => response.json())
             .then(data => {
                 // Filter products that belong to the drinkware category for standard users
-                const filteredProducts = isAdmin ? data : data.filter(product => product.category_id === 1);
+                const filteredProducts = data.filter(product => product.category_id === 1);
                 setProducts(filteredProducts);
             })
             .catch(error => console.error('Error fetching product data:', error));
-    }, [isAdmin]);
+    }, []);
 
     return (
         <div>
             <h2>Drinkware</h2>
             <div className="product-cards">
 
-                {/* If isAdmin is true, display a button to add a new product */}
-                {isAdmin && <button>Add New Product</button>}
-
                 {products.map(product => (
-                    <div key={product.id} className="product-card">
-                        <h3>{product.product_name}</h3>
-                        <p>Price: ${product.price}</p>
-                        <p>Stock: {product.stock}</p>
-                        <img src={`/images/${product.image}`} alt={product.product_name} />
+                    <div className='overall'>
+                        <div className='productContainer'>
+                            <div key={product.id} className="productCard">
+                                <h3>{product.product_name}</h3>
+                                <Img className='cardImage' src={`/images/${product.image}`} alt={product.product_name} />
 
-                        {/* Find dropdown component in bootstrap and allow users to select a size */}
-                        <button>Add to Cart</button>
-                        {/* Add additional dropdowns to select font style, color, etc... Would need to integrate into database models */}
+                                {/* Find dropdown component in bootstrap and allow users to select a size */}
+                                {/* Add additional dropdowns to select font style, color, etc... Would need to integrate into database models */}
+                                <button className='productBtn'>Add to Cart</button>
+                            </div>
+                        </div>
+                        <div className='productInfo'>
+                            <div className='productPrice'>
+                                <p>Price: ${product.price}</p>
+                            </div>
+                            <div>
+                                <p> | </p>
+                            </div>
+                            <div className='productStock'>
+                                <p>Stock: {product.stock}</p>
+                            </div>
+                        </div>
                     </div>
                 ))}
             </div>
